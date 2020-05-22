c733bb6dbb880335f6e1a055ffd653aafd275285
Francisco <francisco@codacy.com>
2020-05-22 16:57:11 +0100

    doc: Try to fetch plugins docs from README

--- src/docs/util/rule.ts
+++ src/docs/util/rule.ts
@@ -19,17 +19,9 @@ export interface Rule {
 /* Get information for a rule at `filePath`. */
 function ruleSync(filePath: string): Rule | undefined {
   const ruleId = path.basename(filePath);
-  const indexPath = path.join(filePath, 'index.js');
-  const code = fs.existsSync(indexPath)
-    ? fs.readFileSync(path.join(filePath, 'index.js'), 'utf-8')
-    : '';
-  const tags = dox.parseComments(code)[0].tags;
 
-  const possibleDescription = find(tags, 'fileoverview');
+  const [title, description] = getTitleAndDescription(filePath);
 
-  const description = possibleDescription
-    ? strip(possibleDescription)
-    : getDescriptionFromPackage(filePath);
   const descriptionLines = description.split('\n');
   const optionsLine = descriptionLines.find((line) =>
     line.startsWith('Options')
@@ -39,7 +31,7 @@ function ruleSync(filePath: string): Rule | undefined {
     defaultValue: findDefault(optionsLine),
     description: trim(description),
     ruleId,
-    title: trim(description).split('\n')[0]
+    title
   };
 }
 
@@ -52,18 +44,58 @@ function findDefault(optionsLine?: string): DefaultValue {
   return;
 }
 
+function getTitleAndDescription(filePath: string): readonly [string, string] {
+  const indexPath = path.join(filePath, 'index.js');
+
+  const code = fs.existsSync(indexPath)
+    ? fs.readFileSync(indexPath, 'utf-8')
+    : '';
+
+  const tags = dox.parseComments(code)[0].tags;
+
+  const possibleDescription = find(tags, 'fileoverview');
+
+  if(possibleDescription) {
+    const commentDescription = strip(possibleDescription)
+    return [trim(commentDescription).split('\n')[0], commentDescription]
+  }
+
+  const description = getDescriptionFromReadme(filePath);
+  const title = getPackageJsonDescription(filePath)
+
+  return [title, description];
+}
+
 // just used to deserialize package.json
 interface PackageValues {
   readonly description: string;
 }
 
-function getDescriptionFromPackage(filePath: string): string {
+function getPackageJsonDescription(filePath: string): string {
   const packageValue = fs.readFileSync(
     path.join(filePath, 'package.json'),
     'utf-8'
   );
   const parsedValue = JSON.parse(packageValue) as PackageValues;
   return parsedValue.description;
+}
+
+function getDescriptionFromReadme(filePath: string): string {
+  const readmePath = path.join(filePath, 'README.md');
+  if (fs.existsSync(readmePath)) {
+    const readme = fs.readFileSync(path.join(filePath, 'README.md'));
+    const firstNewLine = readme.indexOf('\n');
+    const firstTitle = readme.indexOf('\n## ');
+    return readme
+      .slice(firstNewLine + 1, firstTitle)
+      .toString()
+      .trim()
+      .split('\n')
+      .filter((line) => !line.startsWith('[!')) // remove badges
+      .join('\n');
+  }
+
+  return getPackageJsonDescription(filePath);
 }
 
 export default ruleSync;
